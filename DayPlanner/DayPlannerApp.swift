83aa4d18799384599ebec7bf5e833c40248cb7a1
iam-dushyant <dushyant.khatri@icloud.com>
2024-10-08 22:01:42 +0100

    Initial commit

---
+++ DayPlanner/DayPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayPlannerApp.swift
+//  DayPlanner
+//
+//  Created by Dushyant Khatri on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DayPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DayPlannerTests/DayPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DayPlannerTests.swift
+//  DayPlannerTests
+//
+//  Created by Dushyant Khatri on 08/10/2024.
+//
+
+import Testing
+@testable import DayPlanner
+
+struct DayPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
